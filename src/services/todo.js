ad2c9704572e2aeee2888ddad1a2fdb605357928
Utkarsh Dixit <utkarshdix02@gmail.com>
2022-11-16 18:56:09 +0530

    Break tests

--- src/services/todo.js
+++ src/services/todo.js
@@ -6,21 +6,7 @@ import update from 'immutability-helper';
  */
 export function getAll() {
     return [
-        {
-            id: 1,
-            text: 'Learn Javascript',
-            completed: false
-        },
-        {
-            id: 2,
-            text: 'Learn React',
-            completed: false
-        },
-        {
-            id: 3,
-            text: 'Build a React App',
-            completed: false
-        }
+
     ]
 }
 
